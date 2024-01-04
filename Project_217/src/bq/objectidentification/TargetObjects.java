d722a8cda2d89526b842fdec8f35672ade2427dc
sunilreddyg <sunilreddy.gajjala@gmail.com>
2024-01-04 12:21:45 +0530

    Add files via upload

--- Project_217/src/bq/objectidentification/TargetObjects.java
+++ Project_217/src/bq/objectidentification/TargetObjects.java
@@ -1,5 +1,6 @@
 package objectidentification;
 
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,11 +15,16 @@ public class TargetObjects
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://facebook.com");
 		driver.manage().window().maximize();
+		//Manage backup time upto 30 seconds
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
 		driver.findElement(By.id("email")).sendKeys("darshan");
 		driver.findElement(By.id("pass")).sendKeys("hello@123");
 		driver.findElement(By.name("login")).click();
 		
+		
+		
+
 	}
 
 }
